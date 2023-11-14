b3ad7499fc239a325d9f0c9056a3626ed32e30d6
song-richard <rsong1993@gmail.com>
2023-11-14 11:44:15 -0800

    Added questions to questions object

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,53 @@ const inquirer = require('inquirer')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of Project:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description:'
+    },
+    {
+        type: 'input',
+        name: 'table',
+        message: 'Table of Contents:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'License:'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Contributors:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Questions:'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
